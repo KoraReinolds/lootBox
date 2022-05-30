842238f5d67d1d1fd255e660cc074ef3f625d9ef
KoraReinolds <s.shurin94@gmail.com>
2022-05-30 22:29:49 +0300

    add inptRange component - in props passed list of values - eventually emitted chosen value

---
+++ src/components/InputRange.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    v-if="list.length"
+  >
+    <input
+      type='range'
+      min='0'
+      :max="list.length - 1"
+      v-model="listIndex"
+      :disabled="disabled"
+    >
+  </div>
+</template>
+
+<script setup>
+import {
+  defineProps, defineEmits, ref, watch,
+} from 'vue';
+
+const props = defineProps({
+  list: {
+    type: Array,
+  },
+  modelValue: {
+    type: String,
+  },
+  disabled: {
+    type: Boolean,
+  },
+});
+const listIndex = ref(props.list.indexOf(props.modelValue));
+const emit = defineEmits(['update:modelValue']);
+
+watch(listIndex, (index) => {
+  emit('update:modelValue', `${props.list[index]}`);
+});
+</script>
