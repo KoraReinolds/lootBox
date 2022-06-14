7692150fdc81961e2ce2228c748ba8ae44a1db4e
KoraReinolds <s.shurin94@gmail.com>
2022-06-14 13:05:23 +0300

    add delay to animation

--- src/composables/animation/index.js
+++ src/composables/animation/index.js
@@ -28,26 +28,59 @@ export default () => {
   let start = new Date();
 
   const powFunction = ({
-    duration, from, to, pow = 0.3,
+    duration, from, to, pow = 0.3, delay = 0,
   }) => {
-    if (new Date() - start > duration) start = new Date();
-
-    const delta = new Date() - start;
+    const delta = new Date() - start - delay;
     const ratio = delta / duration;
 
     return from + (to - from) * (ratio ** pow);
   };
 
   const animate = ({
-    functionName, ...params
+    mesh, ...functions
   }) => {
-    const animationFunction = animationFunctions[functionName];
+    const timeFromStart = new Date() - start;
 
-    if (!animationFunction) return;
+    Object.entries(functions).forEach(([functionName, { params = {}, ...animationData }]) => {
+      const animationFunction = animationFunctions[functionName];
+      const { delay = 0 } = params;
+      const gaps = Object.keys(animationData);
+      const values = Object.values(animationData);
 
-    animationFunction({
-      ...params,
-      value: powFunction(params),
+      if (!animationFunction) {
+        console.warn(`can't find animation function with name ${functionName}`);
+      }
+
+      if (timeFromStart < delay) {
+        animationFunctions.opacityChange({
+          mesh, value: 0,
+        });
+      } else {
+        let currentGapIndex = +gaps.findIndex(
+          (time) => +time > timeFromStart,
+        );
+
+        if (currentGapIndex === -1) {
+          start = new Date();
+          currentGapIndex = 1;
+        }
+
+        const to = values[currentGapIndex];
+        const from = values[currentGapIndex - 1];
+        const duration = +gaps[currentGapIndex] - +gaps[currentGapIndex - 1];
+
+        const functionParams = {
+          ...params,
+          mesh,
+          to,
+          from,
+          duration,
+        };
+        animationFunction({
+          ...functionParams,
+          value: powFunction(functionParams),
+        });
+      }
     });
   };
 
@@ -57,47 +90,50 @@ export default () => {
       textMeshes.forEach((wrappedMesh, index) => {
         animationFunctions.positionChange({
           mesh: wrappedMesh.children[0],
-          duration: 2000,
           axis: ['z'],
           value: 100,
         });
 
+        const delay = index * 300;
+
         animate({
           mesh: wrappedMesh,
-          duration: 2000,
-          functionName: 'rotationChange',
-          axis: ['y'],
-          from: index % 2 ? -Math.PI / 4 : Math.PI / 4,
-          to: 0,
+          rotationChange: {
+            params: { axis: ['y'], delay },
+            0: index % 2 ? -Math.PI / 4 : Math.PI / 4,
+            2000: 0,
+          },
+          opacityChange: {
+            params: { delay },
+            0: 0,
+            2000: 1,
+          },
         });
       });
     },
 
-    type2: ({ group }) => {
-      animate({
-        mesh: group,
-        duration: 2000,
-        functionName: 'positionChange',
-        axis: ['y'],
-        from: -80,
-        to: -180,
-      });
+    type2: ({ textMeshes }) => {
+      textMeshes.forEach((wrappedMesh, index) => {
+        const delay = index * 300;
 
-      animate({
-        mesh: group,
-        duration: 2000,
-        functionName: 'scaleChange',
-        pow: 0.15,
-        from: 0,
-        to: 1,
-      });
-
-      animate({
-        mesh: group,
-        duration: 2000,
-        functionName: 'opacityChange',
-        from: 0,
-        to: 1,
+        animate({
+          mesh: wrappedMesh,
+          positionChange: {
+            params: { axis: ['y'], delay },
+            0: -80 * index - 100,
+            2000: -80 * index,
+          },
+          scaleChange: {
+            params: { pow: 0.15, delay },
+            0: 0,
+            2000: 1,
+          },
+          opacityChange: {
+            params: { delay },
+            0: 0,
+            2000: 1,
+          },
+        });
       });
     },
   };
